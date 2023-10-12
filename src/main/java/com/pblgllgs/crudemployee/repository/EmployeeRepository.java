04bdbf41529e99ec91d997390247bae3ecc487b8
pbl.gllgs <pbl.gllgs@gmail.com>
2023-10-12 18:44:42 -0300

    data-rest pagination, size, sort

--- src/main/java/com/pblgllgs/crudemployee/repository/EmployeeRepository.java
+++ src/main/java/com/pblgllgs/crudemployee/repository/EmployeeRepository.java
@@ -2,8 +2,9 @@ package com.pblgllgs.crudemployee.repository;
 
 import com.pblgllgs.crudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
